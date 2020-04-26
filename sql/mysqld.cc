81d60c9813135780c5ef87bb331cd4a25d7b5b9e
Marko Mäkelä <marko.makela@mariadb.com>
2020-04-26 22:39:19 +0300

    MDEV-22203: WSREP_ON is unnecessarily expensive to evaluate
    This is a backport of the applicable part of
    commit 93475aff8de80a0ef53cbee924bcb70de6e86f2c and
    commit 2c39f69d34e64a5cf94720e82e78c0ee91bd4649
    from 10.4.
    
    Before 10.4 and Galera 4, WSREP_ON is a macro that points to
    a global Boolean variable, so it is not that expensive to
    evaluate, but we will add an unlikely() hint around it.
    
    WSREP_ON_NEW: Remove (unused macro).
    
    Replace some use of WSREP_ON with WSREP(thd), like it was done
    in 93475aff8de80a0ef53cbee924bcb70de6e86f2c. Note: the macro
    WSREP() in 10.1 is equivalent to WSREP_NNULL() in 10.4.
    
    Item_func_rand::seed_random(): Avoid invoking current_thd
    when WSREP is not enabled.

--- sql/mysqld.cc
+++ sql/mysqld.cc
@@ -1,5 +1,5 @@
 /* Copyright (c) 2000, 2015, Oracle and/or its affiliates.
-   Copyright (c) 2008, 2018, MariaDB
+   Copyright (c) 2008, 2020, MariaDB
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -5772,8 +5772,10 @@ int mysqld_main(int argc, char **argv)
       set_user(mysqld_user, user_info);
   }
 
+#ifdef WITH_WSREP
   if (WSREP_ON && wsrep_check_opts())
     global_system_variables.wsrep_on= 0;
+#endif
 
   if (opt_bin_log && !global_system_variables.server_id)
   {
